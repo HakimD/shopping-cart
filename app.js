a46a2abeaba24820a2bb5aa34e25eafcdeeffedb
HkmD <djabou.hakim@gmail.com>
2018-12-02 22:34:53 +0100

    seeding data

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var expressHbs = require('express-handlebars');
-
+var mongoose = require('mongoose');
 var routes = require('./routes/index');
 
 var app = express();
-
+mongoose.connect('mongodb://localhost:27017/shopping', {useNewUrlParser: true } )
 // view engine setup
 app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
 app.set('view engine', '.hbs');
